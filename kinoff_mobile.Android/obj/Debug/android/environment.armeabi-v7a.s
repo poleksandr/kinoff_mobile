19c4014a01f4ecb04a96c6277112ac6d04a66eb5
Robert Smid <smidro17@sps-prosek.cz>
2020-12-15 15:14:42 +0100

    MainPage

--- kinoff_mobile.Android/obj/Debug/android/environment.armeabi-v7a.s
+++ kinoff_mobile.Android/obj/Debug/android/environment.armeabi-v7a.s
@@ -92,7 +92,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"13fabac2-59b2-40a6-ac19-258de48deca3"
+	.asciz	"544645b7-c472-4802-914b-50f49e036601"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
